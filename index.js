c4a8aa7753ae90eb44168a776654af17cfd5a08f
siratl <elishubax@gmail.com>
2019-02-25 14:50:05 -0800

    completed guided mini project

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const bcrypt = require('bcryptjs');
 
 const db = require('./database/dbConfig.js');
 const Users = require('./users/users-module.js');
@@ -18,8 +19,9 @@ server.get('/', (req, res) => {
 server.post('/api/register', (req, res) => {
   let user = req.body;
   // generate hash from user's password
-
+  const hash = bcrypt.hashSync(user.password, 16);
   // override user.password with hash
+  user.password = hash;
 
   Users.add(user)
     .then(saved => {
@@ -37,7 +39,8 @@ server.post('/api/login', (req, res) => {
     .first()
     .then(user => {
       // check that passwords match
-      if (user) {
+
+      if (user && bcrypt.compareSync(password, user.password)) {
         res.status(200).json({ message: `Welcome ${user.username}!` });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
@@ -48,8 +51,33 @@ server.post('/api/login', (req, res) => {
     });
 });
 
+function restricted(req, res, next) {
+  const { username, password } = req.headers;
+
+  if (username && password) {
+    Users.findBy({ username })
+      .first()
+      .then(user => {
+        // check that passwords match
+        if (user && bcrypt.compareSync(password, user.password)) {
+          next();
+        } else {
+          res.status(401).json({ message: 'Invalid Credentials' });
+        }
+      })
+      .catch(error => {
+        res.status(500).json({ message: 'Unexpected Error!' });
+      });
+  } else {
+    res.status(400).json({ message: 'No credentials provided' });
+  }
+}
+
+// getting Headers On AXIOS
+//axios.get(url, { headers: { username, password }})
+
 // protect this route, only authenticated users should see it
-server.get('/api/users', (req, res) => {
+server.get('/api/users', restricted, (req, res) => {
   Users.find()
     .then(users => {
       res.json(users);
